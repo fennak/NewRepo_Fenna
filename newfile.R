c209c9c66508ac7b5cf9d9b9c6ccd3b4ab509eec
fennak <krienen@vpn6-110.broadinstitute.org>
2016-04-07 14:08:57 -0400

    This is the first file in my NewRepo_Fenna, committed to branch1

---
+++ newfile.R
@@ -0,0 +1,3 @@
+# here's a new file, see if it appears under branch1 in NewRepo_Fenna
+
+# this was created by going to Repository tab in GitHub desktop, Open in Atom, and creating a new file (called "newfile.R")
