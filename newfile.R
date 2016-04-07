afbdcf0a9bd66cb7a1a01cc7b3803a5a91d2c650
fennak <krienen@vpn6-110.broadinstitute.org>
2016-04-07 14:21:14 -0400

    i'm committing a new change to newfile.R to branch1.
    will this change be excluded from branch1.1?

--- newfile.R
+++ newfile.R
@@ -1,3 +1,5 @@
 # here's a new file, see if it appears under branch1 in NewRepo_Fenna
 
 # this was created by going to Repository tab in GitHub desktop, Open in Atom, and creating a new file (called "newfile.R")
+
+# i have since created branch1.1. now i want to see what happens when i update this first file. Will it also show up in branch1.1?
