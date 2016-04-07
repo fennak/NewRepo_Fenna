1a4bfe0051391d23e3f2e6d4dac4dd047ec91775
fennak <krienen@vpn6-110.broadinstitute.org>
2016-04-07 15:46:36 -0400

    Merge branch 'arpy-v1-branch1' into branch1
    # Conflicts:
    #	newfile.R

--- newfile.R
+++ newfile.R
@@ -2,4 +2,10 @@
 
 # this was created by going to Repository tab in GitHub desktop, Open in Atom, and creating a new file (called "newfile.R")
 
+<<<<<<< HEAD
 # i have since created branch1.1. now i want to see what happens when i update this first file. Will it also show up in branch1.1?
+=======
+# Okay this is me - Arpy - Trying to edit the damn file
+
+# Fenna has synced her GH Desktop and now I see arpy-v1-branch1 underneath branch1
+>>>>>>> arpy-v1-branch1
